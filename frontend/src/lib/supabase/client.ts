7e0098f1c743e53b54c24d2ca9668927760c3626
atulsinghhhh <atulsinghhh25@gmail.com>
2026-05-05 19:02:17 +0530

    fixing lint issue

--- frontend/src/lib/supabase/client.ts
+++ frontend/src/lib/supabase/client.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// @ts-ignore
 const supabaseUrl = import.meta.env?.BUN_PUBLIC_SUPABASE_URL || process.env.BUN_PUBLIC_SUPABASE_URL;
-// @ts-ignore
 const supabasePublishableKey = import.meta.env?.BUN_PUBLIC_SUPABASE_PUBLISHABLE_KEY || process.env.BUN_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
 console.log("SUPABASE URL:", supabaseUrl);
