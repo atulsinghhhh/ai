b777109c16b9b57e7b4b4ec86300e801c95673b8
atulsinghhhh <atulsinghhh25@gmail.com>
2026-05-06 15:00:28 +0530

    add frontend_url in index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -10,7 +10,10 @@ import { prisma } from "./db";
 
 const app = express()
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: [
+        "http://localhost:3000",
+        process.env.FRONTEND_URL
+    ].filter(Boolean) as string[],
     credentials: true,
 }))
 
